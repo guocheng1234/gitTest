3d47fb0fadc92f177fafd0fa63bb13f31d789a18
caiguocheng <244122535@qq.com>
2019-03-31 15:31:05 +0800

    1

--- src/test/PassengerInterface.java
+++ src/test/PassengerInterface.java
@@ -1,5 +1,33 @@
 package test;
 
-public interface PassengerInterface {
-
-}
+/** 
+  * 旅客接口 
+  */ 
+ public interface PassengerInterface { 
+     /** 
+      * 获取旅客姓名 
+      * @return 返回姓名 
+      */ 
+     public String getName(); 
+ 
+ 
+     /** 
+      * 获取预定号 
+      * @return 返回预订号 
+      */ 
+     public int getBookingNumber(); 
+ 
+ 
+     /** 
+      * 获取座位排数 
+      * @return 返回排数 
+      */ 
+     public int getRow(); 
+ 
+ 
+     /** 
+      * 获取座位号 
+      * @return 返回座位号 
+      */ 
+     public int getSeatPosition(); 
+ } 
